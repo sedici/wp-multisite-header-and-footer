89e195d9013d00c6d5ac1883f307c3fc2b6fc1f4
Lautaro <ljosinsaller@gmail.com>
2026-05-28 07:47:06 -0300

    feature : implemento métodos en clase deactivator e incluyo un Trait multisite_helper en ambas clases

--- core/class-activator.php
+++ core/class-activator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace SediciMultisiteFooter\Core;
+use SediciMultisiteFooter\Inc\Multisite_Helper;
 
 /**
  * Acciones luego de desactivar el plugin
@@ -8,9 +9,10 @@ namespace SediciMultisiteFooter\Core;
 
 class Activator {
 
+    use Multisite_Helper;
+
 	/** 
 	 * Acciones a ejecutar luego de activar el plugin
-     * Incluye 2 options : uno para conocer el estado del footer y otro para obtener el tipo de footer seteado
      * @return void
      * 
 	 */
@@ -37,32 +39,6 @@ class Activator {
     public static function register_site_options_in_database($default_type = 'prebi-sedici') {
         add_option('sedici_footer_status', 0);
         add_option('sedici_footer_type', $default_type);
-    }
-
-	/**
-     * Ejecuta una función específica (callback) en todos los sitios de la red multisitio.
-     *
-     * @param callable $callback La función a ejecutar dentro del contexto de cada sitio.
-     * @return void
-     */
-    private static function run_on_all_sites( callable $callback ) {
-        if ( ! is_multisite() ) {
-            return; // Seguridad: Si no es multisitio, salimos.
-        }
-
-        $blog_id_actual = get_current_blog_id();
-        $sitios = get_sites();
-
-        foreach ( $sitios as $sitio ) {
-            switch_to_blog( $sitio->blog_id );
-            
-            // Ejecutamos la función que nos pasaron por parámetro
-            call_user_func( $callback );
-            
-            restore_current_blog();
-        }
-
-        switch_to_blog( $blog_id_actual );
     }
 
 }
--- core/class-deactivator.php
+++ core/class-deactivator.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace SediciMultisiteFooter\Core;
+use SediciMultisiteFooter\Inc\Multisite_Helper;
 
 /**
  * Acciones luego de desactivar el plugin
  */
 
 class Deactivator {
+
+    use Multisite_Helper;
 
 	/**
 	 *  Acciones a ejecutar luego de desactivar el plugin
@@ -16,47 +19,26 @@ class Deactivator {
 	public static function deactivate() {
 
 		if ( is_multisite() ) {
-            delete_network_option(get_current_network_id(),'sedici_footer_network_status');
-            delete_network_option(get_current_network_id(),'sedici_footer_network_type');
+            self::delete_network_options_from_database();
 			self::run_on_all_sites( function() {
-				delete_option('sedici_footer_status');
-                delete_option('sedici_footer_type');
+                self::delete_site_options_from_database();
 			});
         }
 		else {
-			delete_option('sedici_footer_status');
-            delete_option('sedici_footer_type');
+			self::delete_site_options_from_database();
 		}
 	
 	}
 
 
-    public static function delete_options_from_database();
+    public static function delete_network_options_from_database() {
+        delete_network_option(get_current_network_id(),'sedici_footer_network_status');
+        delete_network_option(get_current_network_id(),'sedici_footer_network_type');
+    }
 
-	/**
-     * Ejecuta una función específica (callback) en todos los sitios de la red multisitio.
-     *
-     * @param callable $callback La función a ejecutar dentro del contexto de cada sitio.
-     * @return void
-     */
-    private static function run_on_all_sites( callable $callback ) {
-        if ( ! is_multisite() ) {
-            return; // Seguridad: Si no es multisitio, salimos.
-        }
-
-        $blog_id_actual = get_current_blog_id();
-        $sitios = get_sites();
-
-        foreach ( $sitios as $sitio ) {
-            switch_to_blog( $sitio->blog_id );
-            
-            // Ejecutamos la función que nos pasaron por parámetro
-            call_user_func( $callback );
-            
-            restore_current_blog();
-        }
-
-        switch_to_blog( $blog_id_actual );
+    public static function delete_site_options_from_database() {
+        delete_option('sedici_footer_status');
+        delete_option('sedici_footer_type');
     }
 
 }
