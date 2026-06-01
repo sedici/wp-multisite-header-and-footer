8ce4a622c4b2e208a9043927d671d2d3430d76f0
Lautaro <ljosinsaller@gmail.com>
2026-06-01 10:01:57 -0300

    refactor : elimino metodos en clase init y corrijo metodo reg_public_styles

--- core/class-init.php
+++ core/class-init.php
@@ -50,38 +50,8 @@ class Init {
 	}
 
 	public function reg_public_styles() {
-		$public_css_HYF_url = SEDICI_MULTISITE_FOOTER_PLUGIN_DIR.'templates/css/headerAndFooter.css';
-		$public_css_GENERAL_url = SEDICI_MULTISITE_FOOTER_PLUGIN_DIR.'public/css/sedici-global-footer-public.css';
-		wp_register_style("multisite-manager-general-css", $public_css_GENERAL_url);
+		$public_footer_css = plugins_url( 'public/css/sedici-global-footer-public.css', dirname( __DIR__ ) . '/multisite-footer.php' );		wp_register_style("multisite-manager-general-css", $public_footer_css);
 		wp_enqueue_style("multisite-manager-general-css");
 	}
-
-
-	function get_image_url($post_id) {
-
-		if(get_post_meta(get_the_ID(),'site_screenshot') and (!empty(get_post_meta(get_the_ID(),'site_screenshot')[0]) ))
-		{
-			$image = $this->get_image($post_id,'site_screenshot');
-
-			$image_src = '';
-
-			if(!is_wp_error($image)){
-				$image_src = wp_get_attachment_url($this->get_image($post_id,'site_screenshot')) ;
-		 	} 
-
-			return $image_src;
-
-		}
-	}
-	
-
-	function get_image($post_id,$field){
-		return get_post_meta($post_id, $field,true);
-	}
-
-    
-
-
-
 
 }
